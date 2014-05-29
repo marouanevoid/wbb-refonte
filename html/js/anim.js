46011e55336e3f43b92f50b5821de686bcf0262a
Jerome Barbato <jerome@metabolism.fr>
2014-05-29 08:07:09 -0700

    remove anim.js code

--- html/js/anim.js
+++ html/js/anim.js
@@ -5,16 +5,4 @@ var easing = "easeInOutCubic";
 
 $( document ).ready(function() {
 
-
-// Dropdown
-
-    $('.dropdown').click(function() {
-        if( $('.dropdown').hasClass("dropdown-open") ){
-            $(this).removeClass('dropdown-open').find('.choice').slideDown(speed);
-        }
-        else{
-            $(this).addClass('dropdown-open').find('.choice').slideUp(speed);
-        }
-    });
-// End Dropdown
 });
