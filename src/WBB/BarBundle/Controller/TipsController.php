4aa3af68ecb80efdc333665a4bfd0cb066a1e1a7
Badr HAKKARI <b.hakkari@void.fr>
2014-06-11 09:45:57 +0100

    Fixed Tips errors on add

--- src/WBB/BarBundle/Controller/TipsController.php
+++ src/WBB/BarBundle/Controller/TipsController.php
@@ -3,6 +3,7 @@
 namespace WBB\BarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use WBB\BarBundle\Entity\Tip;
