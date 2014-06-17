c3d161957848d668ca69b2ce5175262153bd549b
Mohamed El Bassir <m.elbassir@void.fr>
2014-06-17 09:44:32 +0100

    Suburb is not required

--- src/WBB/CoreBundle/Controller/Ajax/AjaxController.php
+++ src/WBB/CoreBundle/Controller/Ajax/AjaxController.php
@@ -135,4 +135,28 @@ class AjaxController extends Controller
             'cities' => $response
         ));
     }
+
+    private function arrayTagsToString($tags)
+    {
+        $result = "";
+
+        foreach($tags as $tag)
+        {
+            $result .= $tag->getTag()->getName().', ';
+        }
+
+        return substr($result, 0, -2);
+    }
+
+    private function barFirstImage($bar)
+    {
+        $medias = $bar->getMedias();
+        foreach($medias as $media)
+        {
+            if($media->getMedia()->getProviderName() === "sonata.media.provider.image")
+                return $this->container->get("sonata.media.provider.image")->generatePublicUrl($media->getMedia(), 'slider_large');
+        }
+
+        return null;
+    }
 }
