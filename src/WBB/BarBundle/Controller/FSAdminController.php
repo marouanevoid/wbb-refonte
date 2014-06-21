47c4d6eeefac4229e741ce144b6ef9c380d1528c
Badr HAKKARI <b.hakkari@void.fr>
2014-06-21 13:24:33 +0100

    Loading Tips

--- src/WBB/BarBundle/Controller/FSAdminController.php
+++ src/WBB/BarBundle/Controller/FSAdminController.php
@@ -116,28 +116,34 @@ class FSAdminController extends Controller
         $all    = $this->container->get('tip.repository')->findLatestTips($bar, $offset, 0);
 
         $nbResults  = count($tips);
+
         $difference = count($all) - count($tips);
         $FsTips = array();
-
-        if(($nbResults < $limit) and (!is_null($bar->getFoursquare()) or $bar->getFoursquare() != "")){
+        if(($nbResults < $limit) and !is_null($bar->getFoursquare()) and $bar->getFoursquare() != ""){
             $excluded = $bar->getFsExcludedTips();
             $index = 0;
             $count = $offset;
+            $recursive = 0;
             do{
                 $tmp = $this->get("wbb.fstips.feed")->find($bar->getFoursquare(), $count)['data'];
+                $nbFsTips = count($tmp);
+                if ($nbFsTips <= $index && !$nbFsTips)
+                    break;
                 foreach($tmp as $key => $FsTip){
                     if($index == ($limit - $nbResults) ){
                         $difference = 1;
                         break;
                     }
+
                     if(!in_array($FsTip['id'], $excluded)){
                         $FsTips[] = $tmp[$key];
-                    }else{
-                        $index++;
+                        ++$index;
                     }
                 }
-                $count += $this->container->getParameter('feeds_limit');;
-            }while($index < ($limit - $nbResults));
+                $count += $nbFsTips;
+                $recursive++;
+            }while(($index < ($limit - $nbResults)) and $recursive < 5);
+            $nbResults += $index;
         }
 
         $htmldata = $this->renderView('WBBBarBundle:Bar:tips.html.twig', array(
