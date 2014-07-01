8c928922f960f5d3edd32408b055fba9452fe9a5
Khalid Ahmada <k.ahmada@void.fr>
2014-07-01 16:00:08 +0000

    editing list

--- src/WBB/CoreBundle/Resources/public/js/modules/cities.wbb.js
+++ src/WBB/CoreBundle/Resources/public/js/modules/cities.wbb.js
@@ -142,7 +142,7 @@ wbb.CitiesPage = function () {
             {
                 that.context.$container.find('.scrolls').velocity({opacity:1}, that.config.speed, that.config.easing);
 
-                $selector.show();
+                //$selector.show();
             }, 300);
             that._resize();
         }
