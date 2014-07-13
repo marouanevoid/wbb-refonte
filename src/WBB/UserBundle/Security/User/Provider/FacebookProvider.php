80942fcb9475242fbe4c2f6a66947029ec652fa4
Ayyoub BOUMYA <aybbou@gmail.com>
2014-07-13 00:57:14 +0000

    Fixed the Facebook login behaviour.

--- src/WBB/UserBundle/Security/User/Provider/FacebookProvider.php
+++ src/WBB/UserBundle/Security/User/Provider/FacebookProvider.php
@@ -53,27 +53,9 @@ class FacebookProvider implements UserProviderInterface
     {
         $user = $this->findUserByFbId($username);
 
-        try {
-            $fbdata = $this->facebook->api('/me');
-        } catch (FacebookApiException $e) {
-            $fbdata = null;
-        }
-
-        if (!empty($fbdata)) {
-            if (empty($user)) {
-                $user = $this->userManager->createUser();
-                $user->setEnabled(true);
-                $user->setPassword('');
-            }
-
-            // TODO use http://developers.facebook.com/docs/api/realtime
-            $user->setFBData($fbdata);
-
-            if (count($this->validator->validate($user, 'Facebook'))) {
-                // TODO: the user was found obviously, but doesnt match our expectations, do something smart
-                throw new UsernameNotFoundException('The facebook user could not be stored');
-            }
-            $this->userManager->updateUser($user);
+        if (count($this->validator->validate($user, 'Facebook'))) {
+            // TODO: the user was found obviously, but doesnt match our expectations, do something smart
+            throw new UsernameNotFoundException('The facebook user could not be stored');
         }
 
         if (empty($user)) {
