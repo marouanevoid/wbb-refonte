2d8a7652b2d90015830854b9a07848d42f69188f
Badr HAKKARI <b.hakkari@void.fr>
2014-07-26 12:11:40 +0000

    Register and Profil edit page

--- src/WBB/UserBundle/Form/Type/RegistrationFormType.php
+++ src/WBB/UserBundle/Form/Type/RegistrationFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+use WBB\BarBundle\Entity\Tag;
 
 /**
  * Registration Form Type
@@ -42,19 +43,72 @@ class RegistrationFormType extends BaseType
                 'years' => range(1900, date('Y'))
             ))
             ->add('country')
-
-            ->add('prefCity1')
-            ->add('prefCity2')
-            ->add('prefCity3')
-            ->add('prefBar1')
-            ->add('prefBar2')
-            ->add('prefBar3')
-            ->add('prefDrinkBrand1')
-            ->add('prefDrinkBrand2')
-            ->add('prefDrinkBrand3')
-            ->add('prefCocktails1')
-            ->add('prefCocktails2')
-            ->add('prefCocktails3')
+            ->add('prefCity1', null, array('empty_value' => 'Choose a city'))
+            ->add('prefCity2', null, array('empty_value' => 'Choose a city'))
+            ->add('prefCity3', null, array('empty_value' => 'Choose a city'))
+            ->add('prefBar1', null, array('empty_value' => 'Choose a bar'))
+            ->add('prefBar2', null, array('empty_value' => 'Choose a bar'))
+            ->add('prefBar3', null, array('empty_value' => 'Choose a bar'))
+            ->add('prefDrinkBrand1', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a brand',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_DRINK_BRANDS, true);
+                        }
+                )
+            )
+            ->add('prefDrinkBrand2', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a brand',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_DRINK_BRANDS, true);
+                        }
+                )
+            )
+            ->add('prefDrinkBrand3', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a brand',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_DRINK_BRANDS, true);
+                        }
+                )
+            )
+            ->add('prefCocktails1', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a cocktail',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_BEST_COCKTAILS, true);
+                        }
+                )
+            )
+            ->add('prefCocktails2', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a cocktail',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_BEST_COCKTAILS, true);
+                        }
+                )
+            )
+            ->add('prefCocktails3', 'entity', array(
+                    'class'    => 'WBBBarBundle:Tag',
+                    'required' => false,
+                    'property' => 'name',
+                    'empty_value' => 'Choose a cocktail',
+                    'query_builder' => function ($er) {
+                            return $er->findByType(Tag::WBB_TAG_TYPE_BEST_COCKTAILS, true);
+                        }
+                )
+            )
         ;
     }
 
