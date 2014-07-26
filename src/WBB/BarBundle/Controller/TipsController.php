2e0fb5446638597e4a1989907ebaf7cf68afcd0d
Badr HAKKARI <b.hakkari@void.fr>
2014-07-26 09:46:19 +0000

    Tips by registred user

--- src/WBB/BarBundle/Controller/TipsController.php
+++ src/WBB/BarBundle/Controller/TipsController.php
@@ -24,15 +24,13 @@ class TipsController extends Controller
      */
     public function addAction(Request $request)
     {
-//        $user = $this->getUser();
-//        if (!$user) {
-//            return new JsonResponse(array(
-//                'code' => 403,
-//                'message' => 'User not authenticated !'
-//            ));
-//        }
-
-        $user = $this->get('user.repository')->findOneById(1);
+        $user = $this->getUser();
+        if (!$user) {
+            return new JsonResponse(array(
+                'code' => 403,
+                'message' => 'User not authenticated !'
+            ));
+        }
 
         $tip = new Tip();
         $tip->setUser($user);
