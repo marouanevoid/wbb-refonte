5fe1243aa1d40b6d91ac16acc1e90c806db267e7
Ayyoub BOUMYA <a.boumya@void.fr>
2014-08-09 17:00:22 +0100

    [User][Registration] Facebook hover after login / profile email with light only

--- src/WBB/UserBundle/EventListener/RegistrationListener.php
+++ src/WBB/UserBundle/EventListener/RegistrationListener.php
@@ -26,38 +26,40 @@ class RegistrationListener implements EventSubscriberInterface
     public function sendProfileEmail(FormEvent $event)
     {
         $user = $event->getForm()->getData();
-        $profileUrl = $this->container->get('router')->generate('fos_user_profile_edit', array(), true);
+        if (!$user->getFirstname()) {
+            $profileUrl = $this->container->get('router')->generate('fos_user_profile_edit', array(), true);
 
-        $data = array(
-            'context' => array(
-                'user' => $user,
-                'profileUrl' => $profileUrl
-            ),
-            'fromEmail' => $this->container->getParameter('mailer_email'),
-            'toEmail' => $user->getEmail()
-        );
+            $data = array(
+                'context' => array(
+                    'user' => $user,
+                    'profileUrl' => $profileUrl
+                ),
+                'fromEmail' => $this->container->getParameter('mailer_email'),
+                'toEmail' => $user->getEmail()
+            );
 
-        $twig = $this->container->get('twig');
-        $templateName = 'WBBUserBundle:User:completeProfile.html.twig';
-        $context = $twig->mergeGlobals($data['context']);
-        $template = $twig->loadTemplate($templateName);
-        $subject = $template->renderBlock('subject', $context);
-        $textBody = $template->renderBlock('body_text', $context);
-        $htmlBody = $template->renderBlock('body_html', $context);
+            $twig = $this->container->get('twig');
+            $templateName = 'WBBUserBundle:User:completeProfile.html.twig';
+            $context = $twig->mergeGlobals($data['context']);
+            $template = $twig->loadTemplate($templateName);
+            $subject = $template->renderBlock('subject', $context);
+            $textBody = $template->renderBlock('body_text', $context);
+            $htmlBody = $template->renderBlock('body_html', $context);
 
-        $message = \Swift_Message::newInstance()
-                ->setSubject($subject)
-                ->setFrom($data['fromEmail'])
-                ->setTo($data['toEmail']);
+            $message = \Swift_Message::newInstance()
+                    ->setSubject($subject)
+                    ->setFrom($data['fromEmail'])
+                    ->setTo($data['toEmail']);
 
-        if (!empty($htmlBody)) {
-            $message->setBody($htmlBody, 'text/html')
-                    ->addPart($textBody, 'text/plain');
-        } else {
-            $message->setBody($textBody);
+            if (!empty($htmlBody)) {
+                $message->setBody($htmlBody, 'text/html')
+                        ->addPart($textBody, 'text/plain');
+            } else {
+                $message->setBody($textBody);
+            }
+
+            $this->container->get('mailer')->send($message);
         }
-
-        $this->container->get('mailer')->send($message);
     }
 
 }
