7c8059b0a0b7c4819c7434d20c0d91e4c4e6a06e
Ayyoub BOUMYA <a.boumya@void.fr>
2014-08-14 19:16:09 +0100

    [User] created a confirm email twig file

--- src/WBB/CoreBundle/Resources/public/js/wbb.js
+++ src/WBB/CoreBundle/Resources/public/js/wbb.js
@@ -372,9 +372,6 @@ jQuery(document).ready(function($) {
         });
     }
     if (showConfirmed) {
-        var html = '<div id="success" class="min-height"><div class="text-align-center padding-top-100"><div class="subtitle">CONGRATULATIONS&nbsp;!</div><p class="padding-top-40">You are now registered on World’s Best Bars.</p><p>The "100 Bars" best of has been added to your favorites.</p><p class="padding-bottom-40">You can have a look at your favorite contents in your user profile.</p></div></div>';
-        $('.popin-block').html(html);
-        PopIn.resize($('#register'));
         $('#show-popin').click();
     }
     if (showResettingForm !== "0") {
