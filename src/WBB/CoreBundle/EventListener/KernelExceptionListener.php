eec6955225f99df263978e1a89e7920249a1efdc
Ayyoub BOUMYA <a.boumya@void.fr>
2014-08-23 12:48:36 +0100

    [404] fixed the 404 message

--- src/WBB/CoreBundle/EventListener/KernelExceptionListener.php
+++ src/WBB/CoreBundle/EventListener/KernelExceptionListener.php
@@ -6,15 +6,18 @@ use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class KernelExceptionListener
 {
 
     private $router;
+    private $session;
 
-    public function __construct(RouterInterface $router)
+    public function __construct(RouterInterface $router, SessionInterface $session)
     {
         $this->router = $router;
+        $this->session = $session;
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
@@ -24,6 +27,7 @@ class KernelExceptionListener
         if ($exception instanceof HttpExceptionInterface) {
             if ($exception->getStatusCode() == 404) {
                 $response = new RedirectResponse($this->router->generate('homepage'));
+                $this->session->getFlashbag()->add('wbb-not-found', true);
 
                 $event->setResponse($response);
             }
