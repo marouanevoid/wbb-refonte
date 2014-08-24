84f29f966df2bea95ec3218dc74cf1a6e5d9df69
Ayyoub BOUMYA <aybbou@gmail.com>
2014-08-24 20:55:51 +0100

    [User] fixed password validation

--- src/WBB/UserBundle/Entity/User.php
+++ src/WBB/UserBundle/Entity/User.php
@@ -1254,8 +1254,13 @@ class User extends BaseUser
             if (strlen($this->plainPassword) < 6) {
                 $context->addViolationAt('plainPassword', 'Please confirm a valid password (must contain at least 6 caracters, a number and a letter)');
             } else {
-                if (!preg_match('/[A-Za-z]/', $this->plainPassword) || !preg_match('/[0-9]/', $this->plainPassword)) {
-                    $context->addViolationAt('plainPassword', 'Please confirm a valid password (must contain at least 6 caracters, a number and a letter)');
+                $chars = str_split($this->plainPassword);
+                foreach ($chars as $char) {
+                    $asciiCode = ord($char);
+                    if (!(($asciiCode >= 97 && $asciiCode <= 122) || ($asciiCode >= 65 && $asciiCode <= 90) || ($asciiCode >= 48 && $asciiCode <= 57))) {
+                        $context->addViolationAt('plainPassword', 'Please confirm a valid password (must contain at least 6 caracters, a number and a letter)');
+                        break;
+                    }
                 }
             }
         }
